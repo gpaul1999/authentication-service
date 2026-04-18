34afe8ed8b47ea9256e0873f854bc51fa088f9d1
nvo30 <votannguyen1599@gmail.com>
2026-04-18 15:26:25 +0700

    chagne DB to test deployment

--- src/main/java/com/customsalesite/CustomSaleSiteServiceApplication.java
+++ src/main/java/com/customsalesite/CustomSaleSiteServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationServiceApplication {
+public class CustomSaleSiteServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthenticationServiceApplication.class, args);
+        SpringApplication.run(CustomSaleSiteServiceApplication.class, args);
     }
 }
